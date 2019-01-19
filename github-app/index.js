4e0d285879c399aea6c5f1117e32cfca3a4becda
Arie Milner <amilner42@gmail.com>
2019-01-19 15:23:33 -0800

    Experiment with github webhook timing out

--- github-app/index.js
+++ github-app/index.js
@@ -3,42 +3,52 @@ const createHandler = require('github-webhook-handler');
 
 const env = process.env;
 
-const handler = createHandler({
+const webhookHandler = createHandler({
     path: "/",
     secret: env.WEBHOOK_SECRET || "dev-secret"
 });
 
-http.createServer(function(req, res) {
-    handler(req, res, function(err) {
-        res.statusCode = 404;
-        res.end("No such location");
-    });
-}).listen(env.GITHUB_APP_PORT || 3333);
+http.createServer(handleRequest).listen(env.GITHUB_APP_PORT || 3333);
 
-const fs = require('fs');
-const createApp = require('github-app');
+webHookHandler.on('issues', (event) => {
+  console.log(`Received issue event for "${event.payload.issue.title}"`)
+})
 
-const app = createApp({
-    id: env.GITHUB_APP_ID ||"23724",
-    cert: fs.readFileSync('vivadoc.2019-01-19.private-key.pem')
-});
+function handleRequest (request, response) {
+  // ignore all requests that aren’t POST requests
+  if (request.method !== 'POST') return response.end('ok')
 
-// TODO do I want this?
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-});
+  // here we pass the current request & response to the webHookHandler we created
+  // on top. If the request is valid, then the "issue" above handler is called
+  webHookHandler(request, response, () => response.end('ok'))
+}
 
-handler.on('issues', function (event) {
-  console.log("EVENT", event);
-  if (event.payload.action === 'opened') {
-    var installation = event.payload.installation.id;
-    app.asInstallation(installation).then(function (github) {
-      github.issues.createComment({
-        owner: event.payload.repository.owner.login,
-        repo: event.payload.repository.name,
-        number: event.payload.issue.number,
-        body: 'Welcome to the robot uprising.'
-      });
-    });
-  }
-});
+//
+// const fs = require('fs');
+// const createApp = require('github-app');
+//
+// const app = createApp({
+//     id: env.GITHUB_APP_ID ||"23724",
+//     cert: fs.readFileSync('vivadoc.2019-01-19.private-key.pem')
+// });
+//
+// // TODO do I want this?
+// handler.on('error', function (err) {
+//   console.error('Error:', err.message)
+// });
+//
+// handler.on('issues', function (event) {
+//   console.log("EVENT", event);
+//   if (event.payload.action === 'opened') {
+//     var installation = event.payload.installation.id;
+//     app.asInstallation(installation).then(function (github) {
+//       github.issues.createComment({
+//         owner: event.payload.repository.owner.login,
+//         repo: event.payload.repository.name,
+//         number: event.payload.issue.number,
+//         body: 'Welcome to the robot uprising.'
+//       });
+//     });
+//   }
+// });
+`
