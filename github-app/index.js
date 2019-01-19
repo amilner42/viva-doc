d01df9485c1f3e610c6348255749b343a6049c94
Arie Milner <amilner42@gmail.com>
2019-01-19 15:53:44 -0800

    Retesting base for github app - Forgot to change security group in EC2 to include new port

--- github-app/index.js
+++ github-app/index.js
@@ -8,7 +8,7 @@ const webhookHandler = createHandler({
     secret: env.WEBHOOK_SECRET || "dev-secret"
 });
 
-http.createServer(handleRequest).listen(env.GITHUB_APP_PORT || 3333);
+http.createServer(handleRequest).listen(env.GITHUB_APP_PORT || 3002);
 
 webhookHandler.on('issues', (event) => {
   console.log(`Received issue event for "${event.payload.issue.title}"`)
@@ -23,31 +23,30 @@ function handleRequest (request, response) {
   webHookHandler(request, response, () => response.end('ok'))
 }
 
-//
-// const fs = require('fs');
-// const createApp = require('github-app');
-//
-// const app = createApp({
-//     id: env.GITHUB_APP_ID ||"23724",
-//     cert: fs.readFileSync('vivadoc.2019-01-19.private-key.pem')
-// });
-//
-// // TODO do I want this?
-// handler.on('error', function (err) {
-//   console.error('Error:', err.message)
-// });
-//
-// handler.on('issues', function (event) {
-//   console.log("EVENT", event);
-//   if (event.payload.action === 'opened') {
-//     var installation = event.payload.installation.id;
-//     app.asInstallation(installation).then(function (github) {
-//       github.issues.createComment({
-//         owner: event.payload.repository.owner.login,
-//         repo: event.payload.repository.name,
-//         number: event.payload.issue.number,
-//         body: 'Welcome to the robot uprising.'
-//       });
-//     });
-//   }
-// });
+const fs = require('fs');
+const createApp = require('github-app');
+
+const app = createApp({
+    id: env.GITHUB_APP_ID ||"23724",
+    cert: fs.readFileSync('vivadoc.2019-01-19.private-key.pem')
+});
+
+// TODO do I want this?
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+});
+
+handler.on('issues', function (event) {
+  console.log("EVENT", event);
+  if (event.payload.action === 'opened') {
+    var installation = event.payload.installation.id;
+    app.asInstallation(installation).then(function (github) {
+      github.issues.createComment({
+        owner: event.payload.repository.owner.login,
+        repo: event.payload.repository.name,
+        number: event.payload.issue.number,
+        body: 'Welcome to the robot uprising.'
+      });
+    });
+  }
+});
