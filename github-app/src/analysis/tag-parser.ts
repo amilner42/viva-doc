a78539346095356e444a6fa7e2b7a1ef581bf251
Arie Milner <amilner42@gmail.com>
2019-02-05 21:10:23 -0800

    Change types in tag-parser - Still playing with the types here, will probably change them more   as the ideas for the system get more concrete

--- github-app/src/analysis/tag-parser.ts
+++ github-app/src/analysis/tag-parser.ts
@@ -12,29 +12,83 @@ export type AnalyzeFileParams =
   { type: "deleted", diff: DeletedFileDiff }
 
 
-// All possible VD tag types
-export type VdTag = VdFunctionTag
-export type VdTagType = "function"
-export type VdTagNeedingApproval = VdFunctionTagNeedingApproval
+/** All possible VD tag types
+ *
+ * Tags are the core of VivaDoc, allowing the user to tag some documentation that they'd like to be responsible for.
+ */
+export type VdTag = VdFileTag | VdBlockTag | VdFunctionTag | VdLineTag
 
-// Things that could require approval
-export type FunctionTagApproval = {
-  type: "code-change" | "doc-change" | "tag-change" | "new-tag",
-  lineNumbers: number[]
-}
+// All tag types.
+export type VdTagType = "file" | "block" | "function" | "line"
 
-// Function tags will be responsible for the lines that the function and the function comment
-// take up.
-export interface VdFunctionTag {
-  type: "function";
+// All tags should have these properties.
+export interface BaseTag {
+  type: VdTagType;
   vdTagLineNumber: number;
-  startLineNumber: number;
-  endLineNumber: number;
   owner: string;
 }
 
-export type VdFunctionTagNeedingApproval = VdFunctionTag & {
-  requiresApproval: FunctionTagApproval[];
+// All tags that have line ownership should have these properties.
+export interface LineOwnershipTag {
+  startLineNumber: number;
+  endLineNumber: number;
+}
+
+// A tag representing documentation ownership of an entire file.
+export type VdFileTag = BaseTag & {
+  type: "file";
+}
+
+// A tag representing documentation ownership of a function.
+export type VdFunctionTag = BaseTag & LineOwnershipTag & {
+  type: "function";
+}
+
+// A tag representing documentation ownership of a explicitly specified block.
+export type VdBlockTag = BaseTag & LineOwnershipTag & {
+  type: "block";
+}
+
+// A tag representing documentation ownership of a single line of code.
+export type VdLineTag = BaseTag & LineOwnershipTag & {
+  type: "line";
+}
+
+// A review indicates that a certain `VdTag` must be reviewed.
+export type Review = ReviewNewTag | ReviewDeletedTag | ReviewModifiedTag
+
+// A tag can be new, deleted, or modified in some way.
+export type ReviewType = "new" | "deleted" | "modified"
+
+// All Reviews should have these properties.
+export interface BaseReview {
+  type: ReviewType;
+  tagType: VdTagType;
+}
+
+// A review for a new tag.
+export type ReviewNewTag = BaseReview & {
+  type: "new";
+  isNewFile: boolean;
+  content: string[];
+}
+
+// A review for a deleted tag.
+export type ReviewDeletedTag = BaseReview & {
+  type: "deleted";
+  isDeletedFile: boolean;
+  content: string[];
+}
+
+/** A review for a modified tag.
+
+  Modifications include any/all of the following: changing the tag, changing some code, changing the docs.
+*/
+export type ReviewModifiedTag = BaseReview & {
+  type: "existing";
+  modifiedTag: boolean;
+  modifiedCode: boolean;
+  modifiedDocs: boolean;
 }
 
 /** CONSTANTS */
@@ -44,15 +98,7 @@ const VD_TAG = "@VD"
 /** EXTERNAL FUNCTIONS */
 
 // Returns all line numbers of @VD tags that need to be approved.
-export const analyzeFile = (params: AnalyzeFileParams): VdTagNeedingApproval[] => {
-
-  return getTagsNeedingApproval(params, parseVdTags(params))
-}
-
-/** INERNAL FUNCTIONS */
-
-// TODO
-const getTagsNeedingApproval = (params: AnalyzeFileParams, vdTags: VdTag[]): VdTagNeedingApproval[] => {
+export const analyzeFile = (params: AnalyzeFileParams): Review[] => {
 
   throw new Error("NOT IMPLEMENETED YET")
 }
