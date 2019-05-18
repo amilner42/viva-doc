ec90f1e63845d52901b28df4733bc323c891d046
Arie Milner <amilner42@gmail.com>
2019-05-18 15:50:34 -0700

    Add detection for base branch by searching open PRs - will not perform analysis without an open PR - fixes bug which wasn't pulling files from base branch

--- github-app/src/index.ts
+++ github-app/src/index.ts
@@ -57,75 +57,128 @@ export = (app: Probot.Application) => {
   })
 
   app.on("push", async (context) => {
+
     const pushPayload = context.payload
-    const commits = pushPayload.commits
     const repoId: string = (pushPayload.repository as any).id
     const repoFullName: string = (pushPayload.repository as any).full_name
+    const repoName: string = (pushPayload.repository as any).name
     const branchName: string = (R.last(pushPayload.ref.split("/")) as any) // TODO errors?
-    const numberOfCommits = commits.length
-    const { owner, repo } = context.repo()
-    const baseCommitId: string = pushPayload.before
-    const finalCommitId: string = pushPayload.after
+    const { owner } = context.repo()
+    const headCommitId: string = pushPayload.after
 
-    const lastCommitId = commits[numberOfCommits - 1].id
-    const previousCommitId = (numberOfCommits === 1) ? baseCommitId : commits[numberOfCommits - 2].id
+    const baseBranchInfo = await getBaseBranch(context, repoName, branchName, owner)
+    if (baseBranchInfo === null) { return }
+    const { baseBranchName, baseBranchId } = baseBranchInfo
 
-    const retrieveDiff = async (): Promise<any> => {
-      // Need the correct accept header to get the diff:
-      // Refer: https://developer.github.com/v3/media/#commits-commit-comparison-and-pull-requests
-      const diffAcceptHeader = { accept: "application/vnd.github.v3.diff"}
-      // Unfortunetly the typings are wrong, and they don't include `headers` even though octokit (which this wraps)
-      // has that field, and of course we need to pass the correct header to get the correct media type. This is
-      // why I have a `as any` at the end here, it is only because the typings are wrong.
-      // TODO: Currently assumes base branch is "master", it needs to find the branch
-      return context.github.repos.compareCommits({
-        headers: diffAcceptHeader,
-        owner,
-        repo,
-        base: "master",
-        head: lastCommitId
-      } as any).then(R.path(["data"]))
-    }
-
-    const retrieveFiles = async (previousFilePath: string, currentFilePath: string): Promise<[string, string]> => {
-
-      const getFile = async (commitId: string, path: string): Promise<string> => {
-        return context.github.repos.getContents({
-          owner,
-          repo,
-          path,
-          ref: commitId
-        })
-        .then(R.path<any>(["data"]))
-        .then((data) => {
-          return (new Buffer(data.content, data.encoding)).toString("ascii")
-        })
-      }
-
-
-      const previousFile = await getFile(previousCommitId, previousFilePath)
-      const currentFile = await getFile(lastCommitId, currentFilePath)
-
-      return [ previousFile, currentFile ]
-    }
-
-    const setStatus = async (statusState: "success" | "failure" | "pending", description: string) => {
-      return context.github.repos.createStatus({
-        owner,
-        repo,
-        sha: lastCommitId,
-        context: VIVA_DOC_STATUS_NAME,
-        state: statusState,
-        description
-      }).then(R.path(["data"]))
-    }
-
-
-    Analysis.pipeline(repoId, repoFullName, branchName, finalCommitId, retrieveDiff, retrieveFiles, setStatus)
-    .catch((err) => {
+    Analysis.pipeline(
+      repoId,
+      repoFullName,
+      branchName,
+      headCommitId,
+      () => retrieveDiff(context, owner, repoName, baseBranchName, branchName),
+      retrieveFiles(context, owner, repoName, baseBranchId, headCommitId),
+      setStatus(context, owner, repoName, headCommitId)
+    ).catch((err: any) => {
       console.log(`Analysis Pipeline Error: ${err} --- ${JSON.stringify(err)}`)
     })
 
    })
 
 }
+
+const getBaseBranch = R.curry(
+  async (context: Probot.Context, repoName: string, branchName: string, owner: string)
+  : Promise<null | { baseBranchName: string, baseBranchId: string }> => {
+
+  // TODO check this is good when under an organization?
+  const pullListResponse = await context.github.pulls.list({
+    owner,
+    repo: repoName,
+    state: "open",
+    head: `${owner}:${branchName}`
+  })
+
+  if (pullListResponse.data.length === 0) {
+    return null;
+  }
+
+  if (pullListResponse.data.length > 1) {
+    throw new Error(`Not sure which diff to use with ${pullListResponse.data.length} PRs`)
+  }
+
+  const pullRequest = pullListResponse.data[0]
+
+  return { baseBranchName: pullRequest.base.ref, baseBranchId: pullRequest.base.sha }
+})
+
+const retrieveDiff = R.curry(
+  async ( context: Probot.Context
+  , owner: string
+  , repo: string
+  , baseBranchName: string
+  , headBranchName: string
+  ) : Promise<any> => {
+
+  // Need the correct accept header to get the diff:
+  // Refer: https://developer.github.com/v3/media/#commits-commit-comparison-and-pull-requests
+  const diffAcceptHeader = { accept: "application/vnd.github.v3.diff"}
+  // Unfortunetly the typings are wrong, and they don't include `headers` even though octokit (which this wraps)
+  // has that field, and of course we need to pass the correct header to get the correct media type. This is
+  // why I have a `as any` at the end here, it is only because the typings are wrong.
+  return context.github.repos.compareCommits({
+    headers: diffAcceptHeader,
+    owner,
+    repo,
+    base: baseBranchName,
+    head: headBranchName
+  } as any).then(R.path(["data"]))
+})
+
+const retrieveFiles = R.curry(
+  async ( context: Probot.Context
+  , owner: string
+  , repo: string
+  , baseCommitId: string
+  , headCommitId: string
+  , previousFilePath: string
+  , currentFilePath: string
+  ) : Promise<[string, string]> => {
+
+  const getFile = async (commitId: string, path: string): Promise<string> => {
+    return context.github.repos.getContents({
+      owner,
+      repo,
+      path,
+      ref: commitId
+    })
+    .then(R.path<any>(["data"]))
+    .then((data) => {
+      return (new Buffer(data.content, data.encoding)).toString("ascii")
+    })
+  }
+
+
+  const previousFile = await getFile(baseCommitId, previousFilePath)
+  const currentFile = await getFile(headCommitId, currentFilePath)
+
+  return [ previousFile, currentFile ]
+})
+
+const setStatus = R.curry(
+  async ( context: Probot.Context
+  , owner: string
+  , repo: string
+  , commitId: string
+  , statusState: "success" | "failure" | "pending"
+  , description: string
+  ) => {
+
+  return context.github.repos.createStatus({
+    owner,
+    repo,
+    sha: commitId,
+    context: VIVA_DOC_STATUS_NAME,
+    state: statusState,
+    description
+  }).then(R.path(["data"]))
+})
