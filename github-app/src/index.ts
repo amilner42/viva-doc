8090c43cf6d2164a1e57ed77a1fd5305141e771e
Arie Milner <amilner42@gmail.com>
2019-05-29 16:12:28 -0700

    Remove setting master as protected branch on app.installation webhook

--- github-app/src/index.ts
+++ github-app/src/index.ts
@@ -33,34 +33,34 @@ export = (app: Probot.Application) => {
   // On installation going to set the default branch to be protected.
   // @PROD This will not be the solution in production, but rather probably refer to the config file to figure out which
   // branches they'd like to integrate with VD.
-  app.on("installation.created", async (context) => {
-    const repos = (await context.github.apps.listRepos({})).data.repositories
-    await R.map((async repo => {
-      const defaultBranch = repo.default_branch
-      const owner = repo.owner.login
-      const repoName = repo.name
-      const branches = (await context.github.repos.listBranches({ owner, repo: repoName })).data
-
-      R.map((branch) => {
-        const branchName = branch.name
-        if (branchName === defaultBranch && branch.protected === false) {
-          // @PROD Probably need to make sure this is just adding a rule and not replacing other existing rules.
-          context.github.repos.updateBranchProtection({
-            owner,
-            repo: repoName,
-            branch: branchName,
-            required_status_checks: {
-              strict: true,
-              contexts: [ VIVA_DOC_STATUS_NAME ]
-            },
-            enforce_admins: true,
-            required_pull_request_reviews: null,
-            restrictions: null
-          })
-        }
-      }, branches)
-    }), repos)
-  })
+  // app.on("installation.created", async (context) => {
+  //   const repos = (await context.github.apps.listRepos({})).data.repositories
+  //   await R.map((async repo => {
+  //     const defaultBranch = repo.default_branch
+  //     const owner = repo.owner.login
+  //     const repoName = repo.name
+  //     const branches = (await context.github.repos.listBranches({ owner, repo: repoName })).data
+  //
+  //     R.map((branch) => {
+  //       const branchName = branch.name
+  //       if (branchName === defaultBranch && branch.protected === false) {
+  //         // @PROD Probably need to make sure this is just adding a rule and not replacing other existing rules.
+  //         context.github.repos.updateBranchProtection({
+  //           owner,
+  //           repo: repoName,
+  //           branch: branchName,
+  //           required_status_checks: {
+  //             strict: true,
+  //             contexts: [ VIVA_DOC_STATUS_NAME ]
+  //           },
+  //           enforce_admins: true,
+  //           required_pull_request_reviews: null,
+  //           restrictions: null
+  //         })
+  //       }
+  //     }, branches)
+  //   }), repos)
+  // })
 
   app.on("push", async (context) => {
 
