3d4f7f125241724ac8a08c3034ab02c28d211261
Arie Milner <amilner42@gmail.com>
2019-06-12 12:16:23 -0700

    Remove all unused parsers

--- github-app/src/languages/index.ts
+++ github-app/src/languages/index.ts
@@ -6,18 +6,14 @@ import * as AST from "./ast"
 import * as AppError from "../error"
 import * as Tag from "../tag"
 
-import * as cpp from "./cplusplus/index"
 import * as javascript from "./javascript/index"
-import * as java from "./java/index"
-import * as python from "./python/index"
-import * as typescript from "./typescript/index"
 
 
 /** EXTERNAL TYPES */
 
 
 // Enum of languages we support
-export type Language = "CPlusPlus" | "Java" | "Javascript" | "Python" | "Typescript"
+export type Language =  "Javascript"
 
 // All possible error types
 export type LanguageParserErrorType = "unsupported-file" | "unsupported-extension"
@@ -32,31 +28,21 @@ export class LanguageParserError extends AppError.ProbotAppError {
   }
 }
 
+
 /** EXTERNAL FUNCTIONS */
+
 
 // TODO DOC
 // TODO probably need the file content to get language as well for cases like SQL or c/c++ header files
 export const getLanguage = (extension: string): Language => {
   switch(extension) {
-    case "cc":
-    case "cpp":
-      return "CPlusPlus"
-
-    case "java":
-      return "Java"
-
     case "js":
       return "Javascript"
-
-    case "py":
-      return "Python"
-
-    case "ts":
-      return "Typescript"
   }
 
   throw new LanguageParserError("unsupported-extension", `No language for file extension: ${extension}`)
 }
+
 
 // Extract the language from the extension or throw an error if we don't support that language.
 export const extractFileType = (filePath: string): Language => {
@@ -77,23 +63,12 @@ export const parse = (language: Language, fileContent: string): AST.ReducedFileA
 
   switch (language) {
 
-    case "CPlusPlus":
-      return cpp.parse(fileContent)
-
-    case "Java":
-      return java.parse(fileContent)
-
     case "Javascript":
       return javascript.parse(fileContent)
 
-    case "Python":
-      return python.parse(fileContent)
-
-    case "Typescript":
-      return typescript.parse(fileContent)
-
   } // end switch
 }
+
 
 // Converts a reduced ast to VD tags
 export const astToTags =
@@ -104,20 +79,8 @@ export const astToTags =
 
   switch (language) {
 
-    case "CPlusPlus":
-      return cpp.astToTags(reducedFileAst, fileContent)
-
-    case "Java":
-      return java.astToTags(reducedFileAst, fileContent)
-
     case "Javascript":
       return javascript.astToTags(reducedFileAst, fileContent)
-
-    case "Python":
-      return python.astToTags(reducedFileAst, fileContent)
-
-    case "Typescript":
-      return typescript.astToTags(reducedFileAst, fileContent)
 
   } // end switch
 }
