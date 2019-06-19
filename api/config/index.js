4fd351f780031a78ebb9f3f2abf5207bf9978af6
Arie Milner <amilner42@gmail.com>
2019-06-19 12:21:58 -0700

    Closes #49

--- api/config/index.js
+++ api/config/index.js
@@ -2,19 +2,53 @@
 
 Currently this module works through environment variables. It's better to use flags because you
 can avoid state-related bugs when spinning up new instances but it's not urgent to switch.
+
+ Env variables to set:
+
+ [ prod ] env.NODE_ENV = 'production'
+ [ prod ] env.VD_WEB_CLIENT_ORIGIN
+ [ prod ] env.VD_MONGODB_URI
+ [ prod ] env.VD_PORT
+ [ prod ] env.VD_COOKIE_SECRET
+
+ [ always ] env.VD_GITHUB_CLIENT_SECRET
+ [ always ] env.VD_GITHUB_CLIENT_ID
+
 */
+
 
 const env = process.env;
 const isProduction = env.NODE_ENV === 'production';
 
-// TODO Make sure all of these are good for production
+// Check prod env variables.
+if ( isProduction ) {
+
+  if ( env.VD_WEB_CLIENT_ORIGIN === undefined
+        || env.VD_MONGODB_URI === undefined
+        || env.VD_PORT === undefined
+        || env.VD_COOKIE_SECRET === undefined ) {
+
+    throw "You have undefined production environment variables!";
+  }
+}
+
+// Check always required variables.
+if ( env.VD_GITHUB_CLIENT_SECRET === undefined || env.VD_GITHUB_CLIENT_ID === undefined) {
+  throw "You have undefined environment variables that are required in dev/prod";
+}
+
+
+const webClientOrigin = isProduction ? env.VD_WEB_CLIENT_ORIGIN : "http://localhost:8080";
+const githubCallbackUrl = `${webClientOrigin}/oauth_redirect`
+
+
 module.exports = {
-  isProduction: isProduction,
-  githubClientSecret: "f514f3d3cf098bd0a7f4a06409ceb183ddc5cd2c",
-  githubClientId: "d887c8b5dea7b99a76af",
-  githubCallbackUrl: "http://localhost:8080/oauth_redirect",
-  mongoDbUri: isProduction ? env.MONGODB_URI : 'mongodb://localhost/viva-doc-dev',
-  port: isProduction ? env.port : 3001,
-  sessionSecret: isProduction ? env.COOKIE_SECRET : 'dev-secret-123',
-  webClientOrigin: isProduction ? env.WEB_CLIENT_ORIGIN : "http://localhost:8080"
+  isProduction,
+  githubClientSecret: env.VD_GITHUB_CLIENT_SECRET,
+  githubClientId: env.VD_GITHUB_CLIENT_ID,
+  githubCallbackUrl,
+  mongoDbUri: isProduction ? env.VD_MONGODB_URI : 'mongodb://localhost/viva-doc-dev',
+  port: isProduction ? env.VD_PORT : 3001,
+  sessionSecret: isProduction ? env.VD_COOKIE_SECRET : 'dev-secret-123',
+  webClientOrigin
 };
---
+++ github-app/src/config.ts
@@ -0,0 +1,39 @@
+/** All config for the application.
+
+Currently this module works through environment variables. It's better to use flags because you
+can avoid state-related bugs when spinning up new instances but it's not urgent to switch.
+*/
+
+// Env variables to set:
+//
+// [ prod ] env.NODE_ENV = 'production'
+// [ prod ] env.VD_WEB_CLIENT_ORIGIN
+// [ prod ] env.VD_MONGODB_URI
+
+
+const env = process.env;
+const isProduction = env.NODE_ENV === 'production';
+
+let webClientOrigin: string;
+let mongoDbUri: string;
+
+if ( isProduction ) {
+
+  if (env.VD_WEB_CLIENT_ORIGIN === undefined || env.VD_MONGODB_URI === undefined) {
+    throw "You have undefined production environement variables."
+  }
+
+  webClientOrigin = env.VD_WEB_CLIENT_ORIGIN;
+  mongoDbUri = env.VD_MONGODB_URI;
+
+} else {
+  webClientOrigin = "http://localhost:8080";
+  mongoDbUri = "mongodb://localhost/viva-doc-dev";
+}
+
+
+export const config = {
+  isProduction,
+  webClientOrigin,
+  mongoDbUri
+}
