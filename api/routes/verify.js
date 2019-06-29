a26a3c81d3169300e4d8ccf6b7eca23b36ab0ab0
Arie Milner <amilner42@gmail.com>
2019-06-29 11:05:53 -0700

    Improved possible race-condition to no longer have a race condition using atomic updates - This was done a hacky way before this PR, now fixed to be proper

--- api/routes/verify.js
+++ api/routes/verify.js
@@ -168,6 +168,14 @@ const updateMatchedBecauseHeadCommitHasNotChanged = async (updateResult, repoId,
 }
 
 
+const updateOk = (updateResult) => {
+
+  if (updateResult.ok !== 1) {
+    throw { httpCode: 500, ...errors.internalServerError };
+  }
+}
+
+
 const updateModifiedOneResult = (updateResult) => {
 
   if (updateResult.ok !== 1 || updateResult.nModified !== 1) {
@@ -225,6 +233,7 @@ module.exports = {
   userHasNotApprovedDocs,
   updateMatchedOneResult,
   updateMatchedBecauseHeadCommitHasNotChanged,
+  updateOk,
   updateModifiedOneResult,
   isArrayOfString,
   isInt
