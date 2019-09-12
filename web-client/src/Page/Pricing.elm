8c33f3409c7e9bf48418789ad5a4c0a06a5d6396
Arie Milner <amilner42@gmail.com>
2019-09-12 08:22:46 -0700

    Fill in pricing page with notice of free-ness and open-source-forver-freeness

--- web-client/src/Page/Pricing.elm
+++ web-client/src/Page/Pricing.elm
@@ -1,6 +1,7 @@
 module Page.Pricing exposing (Model, Msg(..), init, update, view)
 
-import Html exposing (div, text)
+import Html exposing (div, h1, p, text)
+import Html.Attributes exposing (class, style)
 import Session
 
 
@@ -19,7 +20,33 @@ init session =
 
 view : Model -> { title : String, content : Html.Html Msg }
 view model =
-    { title = "Pricing", content = div [] [ text "Pricing Page TODO" ] }
+    { title = "Pricing", content = pricingView }
+
+
+pricingView : Html.Html Msg
+pricingView =
+    div
+        []
+        [ div
+            [ class "section has-text-centered" ]
+            [ h1
+                [ class "title is-2" ]
+                [ text "Alpha Version Pricing" ]
+            , div
+                [ class "columns is-centered" ]
+                [ div
+                    [ class "column is-two-thirds" ]
+                    [ div
+                        [ class "content" ]
+                        [ p [] [ text alphaPricingParagraph1 ]
+                        , p [] [ text alphaPricingParagraph2 ]
+                        , p [] [ text alphaPricingParagraph3 ]
+                        , p [] [ text alphaPricingParagraph4 ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -27,3 +54,25 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+
+alphaPricingParagraph1 : String
+alphaPricingParagraph1 =
+    "Free. Yup, you read that right."
+
+
+alphaPricingParagraph2 : String
+alphaPricingParagraph2 =
+    """VivaDoc is young and values early adopters immensly. Adventurous users willing to try VivaDoc before it is
+    mainstream will be given surprise rewards upon our official release!"""
+
+
+alphaPricingParagraph3 : String
+alphaPricingParagraph3 =
+    """Additionally, VivaDoc will always be free for open source projects of every size! We absolutely
+    love open source and want to give back by improving the entire community's documentation."""
+
+
+alphaPricingParagraph4 : String
+alphaPricingParagraph4 =
+    """ Join the documentation revolution today."""
